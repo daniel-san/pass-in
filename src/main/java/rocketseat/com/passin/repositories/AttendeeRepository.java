e4a4c30c13721f47c63af554f12e349298b6370c
Daniel Lucas dos Santos Borges <dlsbdaniel@gmail.com>
2024-04-06 16:23:51 -0300

    Create event api endpoints

--- src/main/java/rocketseat/com/passin/repositories/AttendeeRepository.java
+++ src/main/java/rocketseat/com/passin/repositories/AttendeeRepository.java
@@ -2,6 +2,10 @@ package rocketseat.com.passin.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import rocketseat.com.passin.domain.attendee.Attendee;
+import rocketseat.com.passin.domain.event.Event;
+
+import java.util.List;
 
 public interface AttendeeRepository extends JpaRepository<Attendee, String> {
+    public List<Attendee> findByEventId(String eventId);
 }
